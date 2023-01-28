13b27235625152ce1f0ec6ffd059ce1a38d1fb0f
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-01-28 20:46:51 +0530

    Changes Updated - 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  a = 'Ankush'
+  
   render() {
     return (
-      <div>App {this.a}</div>
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
     )
   }
 }
