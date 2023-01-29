c183a0a10e6f4b14fb4787612c088694a383d7e4
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-01-29 15:59:16 +0530

    Chnages Updated

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    console.log("Hello I am constructor from News component");
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount(){
+    //console.log("Hello2")
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=28260cebe0464bd680a562169f7e0e5f";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+  }
+
   render() {
+    //console.log("Hello")
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsCaster - Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+            <NewsItem
+              title={element.title?element.title:""}
+              description={element.description?element.description:""}
+              imageUrl={element.urlToImage}
+              newsUrl={element.url}
+            />
+          </div>
+        })}       
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
