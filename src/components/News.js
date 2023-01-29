e3d501c18669122760c529dd3d42211e26e07c7b
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-01-29 17:45:22 +0530

    Chnages Updated

--- src/components/News.js
+++ src/components/News.js
@@ -9,16 +9,46 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page:1
     };
   }
 
   async componentDidMount(){
     //console.log("Hello2")
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=28260cebe0464bd680a562169f7e0e5f";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=28260cebe0464bd680a562169f7e0e5f&page=1";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles,totalResults:parsedData.totalResults})
+  }
+
+  handlePrevClick = async ()=>{
+    console.log('Previous')
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=28260cebe0464bd680a562169f7e0e5f&page=${this.state.page - 1}&pagesize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+
+  handleNextClick = async ()=>{
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+      console.log('Next')
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=28260cebe0464bd680a562169f7e0e5f&page=${this.state.page + 1}&pagesize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    }
   }
 
   render() {
@@ -37,6 +67,11 @@ export class News extends Component {
             />
           </div>
         })}       
+        </div>
+
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
